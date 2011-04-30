6d01854f91699a80422aad4620c02f0dea9429ce
dekosuke/JUN komiyama <deco.elb@gmail.com>
2011-04-30 16:37:59 +0900

    multitouch viewer

--- src/cx/ath/dekosuke/ftbt/imageCatalog.java
+++ src/cx/ath/dekosuke/ftbt/imageCatalog.java
@@ -8,10 +8,18 @@ import android.os.Bundle;
 import android.graphics.*;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.content.Intent;
+import android.util.AttributeSet;
+import android.util.FloatMath;
 import android.util.Log;
 
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.widget.ImageView.ScaleType;
+
 import java.util.ArrayList;
 
 import java.io.File;
@@ -36,6 +44,8 @@ import android.view.Display;
 //Progress Dialog出すため
 import android.app.ProgressDialog;
 import java.lang.Thread;
+
+import cx.ath.dekosuke.ftbt.FutabaCatalogAdapter.ImageGetTask;
 import android.os.Handler;
 import android.os.Message;
 
@@ -76,10 +86,10 @@ public class imageCatalog extends Activity implements Runnable {
 	}
 
 	public void run() {
-		try{ //細かい時間を置いて、ダイアログを確実に表示させる
+		try { // 細かい時間を置いて、ダイアログを確実に表示させる
 			Thread.sleep(100);
-		}catch(InterruptedException e){
-			 //スレッドの割り込み処理を行った場合に発生、catchの実装は割愛
+		} catch (InterruptedException e) {
+			// スレッドの割り込み処理を行った場合に発生、catchの実装は割愛
 		}
 		handler.sendEmptyMessage(0);
 	}
@@ -123,228 +133,15 @@ public class imageCatalog extends Activity implements Runnable {
 		// CircleList.move(1);
 
 		imageCatalogView view = new imageCatalogView(this);
-		view.setOnTouchListener(new FlickTouchListener());
+		view.setCurrentImage();
+		final int FP = ViewGroup.LayoutParams.FILL_PARENT;
+		view.setLayoutParams( new LinearLayout.LayoutParams(FP, FP) );
+		view.setClickable(true);
+		view.setFocusable(true);
+		view.setFocusableInTouchMode(true);
+		view.setScaleType(ScaleType.MATRIX);
+		//view.setOnTouchListener(new FlickTouchListener());
 		setContentView(view);
-	}
-
-	private float lastTouchX;
-	private float currentX;
-	private float lastTouchY;
-	private float currentY;
-
-	private class FlickTouchListener implements View.OnTouchListener {
-		public boolean onTouch(View v, MotionEvent event) {
-			switch (event.getAction()) {
-
-			case MotionEvent.ACTION_DOWN:
-				lastTouchX = event.getX();
-				lastTouchY = event.getY();
-				break;
-
-			case MotionEvent.ACTION_UP:
-				currentX = event.getX();
-				currentY = event.getY();
-				float moveX = currentX - lastTouchX;
-				float moveY = currentY - lastTouchY;
-				if (moveY > 5.0f && moveY * moveY > moveX * moveX) {
-					// 画像を保存する
-					String imgFile = CircleList.get();
-					File file = new File(imgFile);
-					try {
-						SDCard.saveFromURL(file.getName(), new URL(imgFile),
-								false);
-						toast.cancel();
-						toast = Toast.makeText(v.getContext(),
-								"画像" + file.getName() + "を保存しました",
-								Toast.LENGTH_SHORT);
-						toast.show();
-					} catch (Exception e) {
-						Log.i("ftbt", "message", e);
-					}
-				} else if (moveX > 5.0f) {
-					// 前に戻る動作
-					CircleList.move(-1);
-					Log.d("ftbt", "motion prev " + CircleList.pos());
-					((imageCatalogView) v).doDraw();
-				} else if (moveX < -5.0f) {
-					// 次に移動する動作
-					CircleList.move(1);
-					Log.d("ftbt", "motion next " + CircleList.pos());
-					((imageCatalogView) v).doDraw();
-				}
-				break;
-
-			case MotionEvent.ACTION_CANCEL:
-				currentX = event.getX();
-				if (lastTouchX < currentX) {
-					// 前に戻る動作
-				}
-				if (lastTouchX > currentX) {
-					// 次に移動する動作
-				}
-				break;
-			}
-			return true;
-		}
-	}
-
-}
-
-class imageCatalogView extends SurfaceView implements SurfaceHolder.Callback {
-
-	public imageCatalogView(Context context) {
-		super(context);
-
-		getHolder().addCallback(this);
-	}
-
-	public void doDraw() {
-		SurfaceHolder holder = getHolder();
-		// SurfaceViewが作成された時の処理（初期画面の描画等）を記述
-		Canvas canvas = holder.lockCanvas();
-
-		// この間にグラフィック描画のコードを記述する。
-		String imgFile = CircleList.get();
-		setTag(imgFile);
-
-		Context context = getContext();
-		WindowManager wm = ((WindowManager) context
-				.getSystemService(Context.WINDOW_SERVICE));
-		Display display = wm.getDefaultDisplay();
-		int width = display.getWidth();
-		int height = display.getHeight();
-
-		try {
-			canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-			Bitmap bmp = ImageCache.getImage(imgFile);
-			if (bmp == null) { // キャッシュない
-				// ImageCache.asyncSetImage(imgFile, imgFile);
-				ImageGetTask task = new ImageGetTask(this);
-				task.execute(imgFile);
-			} else {
-				Log.d("ftbt", "draw image");
-				float s_x = Math.max(1.0f, (float) bmp.getWidth()
-						/ (float) width);
-				float s_y = Math.max(1.0f, (float) bmp.getHeight()
-						/ (float) height);
-				float scale = Math.max(s_x, s_y);
-				int new_x = (int) (bmp.getWidth() / scale);
-				int new_y = (int) (bmp.getHeight() / scale);
-				bmp = Bitmap.createScaledBitmap(bmp, new_x, new_y, true);
-
-				Paint p = new Paint();
-				canvas.drawBitmap(bmp, 0, 0, p);
-
-				imageCatalog activity = (imageCatalog)context;
-				activity.waitDialog.dismiss();
-			}
-		} catch (Exception e) {
-			// Log.i("ftbt", "message", new Throwable());
-			Log.i("ftbt", "message", e);
-		}
-
-		// この間にグラフィック描画のコードを記述する。
-
-		holder.unlockCanvasAndPost(canvas);
-	}
-
-	// 画像取得用スレッド
-	class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
-		private imageCatalogView image;
-		private String tag;
-		private int id;
-
-		public ImageGetTask(imageCatalogView _image) {
-			image = _image;
-			tag = image.getTag().toString();
-			// ID登録
-			synchronized (imageCatalog.lock) {
-				imageCatalog.LastTaskID += 1;
-				id = imageCatalog.LastTaskID;
-			}
-			Log.d("ftbt", "thread id" + id + " created.");
-		}
-
-		@Override
-		protected Bitmap doInBackground(String... urls) {
-
-			Bitmap bm = null;
-			try {
-				Log.d("ftbt", "getting" + urls[0]);
-				bm = ImageCache.getImage(urls[0]);
-				if (bm == null) { // does not exist on cache
-					ImageCache.setImage(urls[0]);
-					bm = ImageCache.getImage(urls[0]);
-				}
-			} catch (Exception e) {
-				Log.i("ftbt", "message", e);
-			}
-			return bm;
-		}
-
-		// メインスレッドで実行する処理
-		@Override
-		protected void onPostExecute(Bitmap result) {
-			// Tagが同じものが確認して、同じであれば画像を設定する
-			if (image != null && tag != null & tag.equals(image.getTag())) {
-				// image.setImageBitmap(result);
-				try {
-					image.doDraw(); // 再描画
-				} catch (Exception e) {
-					Log.i("ftbt", "message", e);
-				}
-			}
-			Log.d("ftbt", "thread " + id + "end.");
-		}
-
-		@Override
-		protected void onCancelled() {
-			Log.d("ftbt", "スレッドキャンセル id=" + id);
-		}
-
-		private Bitmap MyDecodeStream(InputStream in) {
-			final int IO_BUFFER_SIZE = 4 * 1024;
-			Bitmap bitmap = null;
-			BufferedOutputStream out = null;
-			try {
-
-				in = new BufferedInputStream(in, IO_BUFFER_SIZE);
-
-				final ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
-				out = new BufferedOutputStream(dataStream, IO_BUFFER_SIZE);
-				byte[] b = new byte[IO_BUFFER_SIZE];
-				int read;
-				while ((read = in.read(b)) != -1) {
-					out.write(b, 0, read);
-				}
-				// streamCopy(in, out);
-				out.flush();
-
-				final byte[] data = dataStream.toByteArray();
-				bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-
-			} catch (Exception e) {
-				Log.i("ftbt", "message", e);
-			}
-			return bitmap;
-		}
-	}
-
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-			int height) {
-		// TODO Auto-generated method stub
-		doDraw();
-	}
-
-	public void surfaceCreated(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-		doDraw();
-
-	}
-
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		// TODO Auto-generated method stub
-
 	}
 }
 
---
+++ src/cx/ath/dekosuke/ftbt/imageCatalogView.java
@@ -0,0 +1,179 @@
+package cx.ath.dekosuke.ftbt;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+
+
+class imageCatalogView extends ImageView implements OnTouchListener {
+
+	private static final int NONE = 0;
+	private static final int DRAG = 1;
+	private static final int ZOOM = 2;
+	private Matrix matrix = new Matrix();
+	private int mode = NONE;
+	/** 画像移動用の位置 */
+	private PointF point = new PointF();
+	/** ズーム時の座標 */
+	private PointF middle = new PointF();
+	/** ドラッグ用マトリックス */
+	private Matrix moveMatrix = new Matrix();
+	/** Zoom開始時の二点間距離 */
+	private float initLength = 1;
+
+	public imageCatalogView(Context context) {
+		this(context, null, 0);
+	}
+
+	public imageCatalogView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public imageCatalogView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		matrix = new Matrix();
+		matrix.setScale(1, 1);
+		setOnTouchListener(this);
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+		ImageView view = (ImageView) v;
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			Log.d("ftbt", "mode=DRAG");
+			mode = DRAG;
+			point.set(event.getX(), event.getY());
+			moveMatrix.set(matrix);
+			break;
+		case MotionEvent.ACTION_POINTER_2_UP:
+		case MotionEvent.ACTION_UP:
+			Log.d("ftbt", "mode=NONE");
+			mode = NONE;
+			break;
+		case MotionEvent.ACTION_POINTER_2_DOWN:
+			initLength = getLength(event);
+			if (true) {
+				Log.d("ftbt", "mode=ZOOM");
+				moveMatrix.set(matrix);
+				mode = ZOOM;
+			}
+			break;
+		case MotionEvent.ACTION_MOVE:
+			switch (mode) {
+			case DRAG:
+				matrix.set(moveMatrix);
+				matrix.postTranslate(event.getX() - point.x, event.getY() - point.y);
+				view.setImageMatrix(matrix);
+				break;
+			case ZOOM:
+				if (mode == ZOOM) {
+					float currentLength = getLength(event);
+					middle = getMiddle(event, middle);
+					if (true) {
+						matrix.set(moveMatrix);
+						float scale = filter(matrix,currentLength / initLength);
+						matrix.postScale(scale, scale, middle.x, middle.y);
+						view.setImageMatrix(matrix);	
+					}
+					break;
+				}
+				break;
+			}
+		}
+		return false;
+	}
+	
+	//拡大縮小制限用
+	private float filter(Matrix m, float s){
+		return s;
+	}
+
+	private float getLength(MotionEvent e) {
+		float xx = e.getX(1) - e.getX(0);
+		float yy = e.getY(1) - e.getY(0);
+		return FloatMath.sqrt(xx * xx + yy * yy);
+	}
+
+	private PointF getMiddle(MotionEvent e, PointF p) {
+		float x = e.getX(0) + e.getX(1);
+		float y = e.getY(0) + e.getY(1);
+		p.set(x / 2, y / 2);
+		return p;
+	}	
+	
+	void setCurrentImage(){
+		String imgURL = CircleList.get();
+		setTag(imgURL);
+		ImageGetTask task = new ImageGetTask(this);
+		task.execute(imgURL);
+
+	}
+	
+	// 画像取得用スレッド
+	class ImageGetTask extends AsyncTask<String, Void, Bitmap> {
+		private imageCatalogView image;
+		private String tag;
+		private int id;
+
+		public ImageGetTask(imageCatalogView _image) {
+			image = _image;
+			tag = image.getTag().toString();
+			// ID登録
+			synchronized (imageCatalog.lock) {
+				imageCatalog.LastTaskID += 1;
+				id = imageCatalog.LastTaskID;
+			}
+			Log.d("ftbt", "thread id" + id + " created.");
+		}
+
+		@Override
+		protected Bitmap doInBackground(String... urls) {
+
+			Bitmap bm = null;
+			try {
+				Log.d("ftbt", "getting" + urls[0]);
+				bm = ImageCache.getImage(urls[0]);
+				if (bm == null) { // does not exist on cache
+					ImageCache.setImage(urls[0]);
+					bm = ImageCache.getImage(urls[0]);
+				}
+			} catch (Exception e) {
+				Log.i("ftbt", "message", e);
+			}
+			return bm;
+		}
+
+		// メインスレッドで実行する処理
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			// Tagが同じものが確認して、同じであれば画像を設定する
+			if (image != null && tag != null & tag.equals(image.getTag())) {
+				// image.setImageBitmap(result);
+				try {
+					imageCatalog activity = (imageCatalog)getContext();
+					activity.waitDialog.dismiss();
+					//再描画
+					setImageBitmap(result);
+				} catch (Exception e) {
+					Log.i("ftbt", "message", e);
+				}
+			}
+			Log.d("ftbt", "thread " + id + "end.");
+		}
+
+		@Override
+		protected void onCancelled() {
+			Log.d("ftbt", "スレッドキャンセル id=" + id);
+		}
+
+	}
+}
